2ac8b6d1be14cfc46fcaf8cd2a8744158de8c844
henrikszucs <47735877+henrikszucs@users.noreply.github.com>
2023-03-07 09:50:16 +0100

    working attachment

--- src/android/SMTPClient.java
+++ src/android/SMTPClient.java
@@ -1,113 +1,233 @@
 package com.cordova.smtp.client;
 
-import android.os.Environment;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
-import android.os.StrictMode;
-//import android.widget.Toast;
+import java.util.Date;
+import java.util.Properties;
 
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
-import org.json.JSONObject;
-import org.json.JSONArray;
-import org.json.JSONException;
+import javax.activation.CommandMap;
+import javax.activation.MailcapCommandMap;
+import javax.mail.BodyPart;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.PreencodedMimeBodyPart;
 
-public class SMTPClient extends CordovaPlugin {
-    public final String ACTION_SEND_EMAIL = "cordovaSendMail";
-    private final SMTPClient context = this;
+public class Mail extends javax.mail.Authenticator {
+    private String _user;
+    private String _pass;
 
-    @Override
-    public boolean execute(String action, final JSONArray arg1, final CallbackContext callbackContext) {
-//PluginResult result = new PluginResult(Status.INVALID_ACTION);
-        if (action.equals(ACTION_SEND_EMAIL)) {
-            if (android.os.Build.VERSION.SDK_INT > 9) {
-                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-                        .permitAll().build();
-                StrictMode.setThreadPolicy(policy);
-            }
-            final Handler mHandler = new Handler(Looper.getMainLooper()) {
-                @Override
-                public void handleMessage(Message message) {
-                    // This is where you do your work in the UI thread.
-                    // Your worker tells you in the message what to do.
+    private String[] _to;
+    private String[] _cc;
+	private String[] _bcc;
+    private String _from;
 
-                    /*if (message.what == 0) {
-                        Toast.makeText(cordova.getActivity(),
-                                "Email was sent successfully.",
-                                Toast.LENGTH_LONG).show();
-                    } else if (message.what == 1){
-                        Toast.makeText(cordova.getActivity(),
-                                "Email was not sent.", Toast.LENGTH_LONG)
-                                .show();
-                    }
-                    else{
-                        Toast.makeText(cordova.getActivity(),
-                                "There was a problem sending the email. " + message.obj,
-                                Toast.LENGTH_LONG).show();
-                    }*/
-                }
-            };
-            this.cordova.getThreadPool().execute (new Runnable() {
-                public void run() {
-                    try {
-//String message = arg1.getString(0);
-                        JSONObject json = new JSONObject(arg1.getString(0));
+    private String _port;
+    private String _sport;
 
-                        String state = Environment.getExternalStorageState();
-                        context.sendEmailViaGmail(json);
-                        Message message = mHandler.obtainMessage(0, null);
-                        message.sendToTarget();
-                        callbackContext.success();
-//                        return true;
-                    } catch (JSONException ex) {
-                        ex.printStackTrace();
-                        Message message = mHandler.obtainMessage(2, ex);
-                        message.sendToTarget();
-                        callbackContext.error(ex.getMessage());
-//                        return false;
-                    } catch (Exception e) {
-// TODO Auto-generated catch block
-                        e.printStackTrace();
-                        Message message = mHandler.obtainMessage(2, e);
-                        message.sendToTarget();
-                        callbackContext.error(e.getMessage());
-                    }
-                }
-            });
-        };
-        return true;
+    private String _host;
+
+    private String _subject;
+    private String _body;
+
+    private boolean _auth;
+    private boolean _ssl;
+
+    private boolean _debuggable;
+
+    private Multipart _multipart;
+
+    public Mail() {
+        _host = ""; // default smtp server
+        _port = "587"; // default smtp port
+        _sport = "587"; // default socketfactory port
+
+        _user = ""; // username
+        _pass = ""; // password
+        _from = ""; // email sent from
+        _subject = ""; // email subject
+        _body = ""; // email body
+        _ssl = false;
+
+        _debuggable = false; // debug mode on or off - default off
+        _auth = true; // smtp authentication - default on
+
+        _multipart = new MimeMultipart();
+
+        // There is something wrong with MailCap, javamail can not find a handler for the multipart/mixed part, so this bit needs to be added.
+        MailcapCommandMap mc = (MailcapCommandMap) CommandMap.getDefaultCommandMap();
+        mc.addMailcap("text/html;; x-java-content-handler=com.sun.mail.handlers.text_html");
+        mc.addMailcap("text/xml;; x-java-content-handler=com.sun.mail.handlers.text_xml");
+        mc.addMailcap("text/plain;; x-java-content-handler=com.sun.mail.handlers.text_plain");
+        mc.addMailcap("multipart/*;; x-java-content-handler=com.sun.mail.handlers.multipart_mixed");
+        mc.addMailcap("message/rfc822;; x-java-content-handler=com.sun.mail.handlers.message_rfc822");
+        CommandMap.setDefaultCommandMap(mc);
     }
 
-    private void sendEmailViaGmail(JSONObject json) throws Exception {
+    public Mail(String user, String pass) {
+        this();
 
-        Mail m = new Mail(json.getString("smtpUserName"), json.getString("smtpPassword"));
-        String[] toArr = json.getString("emailTo").split(",");
-        String emailCC = json.optString("emailCC");
-        String[] ccArr = (emailCC.isEmpty()) ? null : emailCC.split(",");
-		String emailBCC = json.optString("emailBCC");
-        String[] bccArr = (emailBCC.isEmpty()) ? null : emailBCC.split(",");
-        m.set_to(toArr);
-        m.set_cc(ccArr);
-		m.set_bcc(bccArr);
-        m.set_host(json.getString("smtp"));
-        m.set_from(json.getString("emailFrom"));
-        m.set_body(json.getString("textBody"));
-        m.set_subject(json.getString("subject"));
+        _user = user;
+        _pass = pass;
+    }
 
-        JSONArray attachments = json.getJSONArray("attachments");
-        if(attachments != null){
-            for(int i=0; i < attachments.length(); i++){
-                String fileFullName = attachments.getString(i);
-                if(fileFullName.contains(":")){
-                    fileFullName = fileFullName.split(":")[1];
-                }
-                m.addAttachment(fileFullName);
+    public boolean send() throws Exception {
+        Properties props = _setProperties();
+
+        if (!_user.equals("") && !_pass.equals("") && _to.length > 0 && !_from.equals("") && !_subject.equals("") && !_body.equals("")) {
+            Session session = Session.getInstance(props, this);
+
+            MimeMessage msg = new MimeMessage(session);
+
+            msg.setFrom(new InternetAddress(_from));
+
+            InternetAddress[] addressTo = new InternetAddress[_to.length];
+            for (int i = 0; i < _to.length; i++) {
+                addressTo[i] = new InternetAddress(_to[i]);
+            }
+            msg.setRecipients(MimeMessage.RecipientType.TO, addressTo);
+
+			if((_cc != null)){
+				InternetAddress[] addressCC = new InternetAddress[_cc.length]; 
+				for (int i = 0; i < _cc.length; i++) { 
+					addressCC[i] = new InternetAddress(_cc[i]); 
+				}
+				msg.setRecipients(MimeMessage.RecipientType.CC, addressCC);
+			}
+			if((_bcc != null)){
+				InternetAddress[] addressBCC = new InternetAddress[_bcc.length]; 
+				for (int i = 0; i < _bcc.length; i++) { 
+					addressBCC[i] = new InternetAddress(_bcc[i]); 
+				}
+				msg.setRecipients(MimeMessage.RecipientType.BCC, addressBCC);
+			}
+			
+            msg.setSubject(_subject);
+            msg.setSentDate(new Date());
+
+            // setup message body
+            BodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setContent(_body, "text/html");
+            _multipart.addBodyPart(messageBodyPart);
+
+            // Put parts in message
+            //msg.setContent(_body,"text/html; charset=utf-8");
+            msg.setContent(_multipart);
+
+            // send email
+            Transport.send(msg);
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void addAttachment(String data) throws Exception {
+        String filetype = data.split("//")[0];
+        String filename = data.split("//")[1];
+        String filedata = data.split("//")[2];
+        BodyPart filePart = new PreencodedMimeBodyPart("base64");
+        filePart.setContent(filedata, filetype);
+        filePart.setFileName(filename);
+
+        _multipart.addBodyPart(filePart);
+    }
+
+    public String[] get_to() {
+        return _to;
+    }
+
+    public void set_to(String[] _to) {
+        this._to = _to;
+    }
+
+	public String[] get_cc() {
+		return _cc;
+	}
+
+	public void set_cc(String[] _cc) {
+		this._cc = _cc;
+	}
+	
+	public String[] get_bcc() {
+		return _bcc;
+	}
+
+	public void set_bcc(String[] _bcc) {
+		this._bcc = _bcc;
+	}
+
+    public void set_ssl(boolean _ssl) {
+        this._ssl = _ssl;
+    }
+
+    public String get_from() {
+        return _from;
+    }
+
+    public void set_from(String _from) {
+        this._from = _from;
+    }
+
+    public void set_auth(boolean _auth) {
+        this._auth = _auth;
+    }
+
+    public void set_port(int _port) {
+        this._port = Integer.toString(_port);
+    }
+
+    public void set_sport(int _sport) {
+        this._sport = Integer.toString(_sport);
+    }
+
+    public void set_host(String _host) {
+        this._host = _host;
+    }
+
+    public String get_body() {
+        return _body;
+    }
+
+    public void set_body(String _body) {
+        this._body = _body;
+    }
+
+    @Override
+    public PasswordAuthentication getPasswordAuthentication() {
+        return new PasswordAuthentication(_user, _pass);
+    }
+
+    private Properties _setProperties() {
+        Properties props = new Properties();
+
+        props.put("mail.smtp.host", _host);
+        props.put("mail.smtp.port", _port);
+        if(_auth){
+            props.put("mail.smtp.auth", "true");
+            if(_ssl){
+                props.put("mail.smtp.socketFactory.port", _sport);
+                props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            }
+            else {
+                props.put("mail.smtp.starttls.enable", "true");
             }
         }
 
-        boolean sendFlag = m.send();
-
+        return props;
     }
 
+    public String get_subject() {
+        return _subject;
+    }
+
+    public void set_subject(String _subject) {
+        this._subject = _subject;
+    }
+
+    // more of the getters and setters
 }
