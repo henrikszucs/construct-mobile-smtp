33bcbb5e09235c10f024776ce864bf28b2193062
henrikszucs <47735877+henrikszucs@users.noreply.github.com>
2023-03-08 19:34:15 +0100

    Update smtpClient.js

--- www/smtpClient.js
+++ www/smtpClient.js
@@ -2,7 +2,17 @@
 
 var smtpClient = {
     sendMail: function (mailSettings, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "SMTPClient", "cordovaSendMail", [ JSON.stringify(mailSettings) ]);
+        const keys = ["smtp", "smtpUserName", "smtpPassword", "emailFrom", "emailTo", "emailCC", "emailBCC", "subject", "textBody", "priority", "attachmentsName", "attachmentsRole", "attachmentsType", "attachmentsBase64"];
+        for (let key of keys) {
+            if (typeof mailSettings?.[key] === "undefined") {
+                throw new Error("\"" + key + "\" parameter is not a found");
+            }
+        }
+        if (mailSettings["smtp"] === "" || mailSettings["smtpUserName"]  === "" || mailSettings["smtpPassword"] === "") {
+            successCallback();
+        } else {
+            cordova.exec(successCallback, errorCallback, "SMTPClient", "cordovaSendMail", [ JSON.stringify(mailSettings) ]);
+        }
     },
     isLoaded: function() {
         console.info('SMTP Client is loaded !');
