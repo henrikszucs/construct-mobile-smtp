4b9d04ca8d8929577cab05f3586ca7615e90da7a
henrikszucs <47735877+henrikszucs@users.noreply.github.com>
2023-03-08 19:36:06 +0100

    Update smtpClient.js

--- www/smtpClient.js
+++ www/smtpClient.js
@@ -1,17 +1,17 @@
 /*global cordova, module*/
 
-var smtpClient = {
-    sendMail: function (mailSettings, successCallback, errorCallback) {
+let smtpClient = {
+    sendMail: function(mailSettings, successCallback, errorCallback) {
         const keys = ["smtp", "smtpUserName", "smtpPassword", "emailFrom", "emailTo", "emailCC", "emailBCC", "subject", "textBody", "priority", "attachmentsName", "attachmentsRole", "attachmentsType", "attachmentsBase64"];
         for (let key of keys) {
             if (typeof mailSettings?.[key] === "undefined") {
                 throw new Error("\"" + key + "\" parameter is not a found");
             }
         }
-        if (mailSettings["smtp"] === "" || mailSettings["smtpUserName"]  === "" || mailSettings["smtpPassword"] === "") {
+        if (mailSettings["smtp"] === "" || mailSettings["smtpUserName"] === "" || mailSettings["smtpPassword"] === "") {
             errorCallback();
         } else {
-            cordova.exec(successCallback, errorCallback, "SMTPClient", "cordovaSendMail", [ JSON.stringify(mailSettings) ]);
+            cordova.exec(successCallback, errorCallback, "SMTPClient", "cordovaSendMail", [JSON.stringify(mailSettings)]);
         }
     },
     isLoaded: function() {
